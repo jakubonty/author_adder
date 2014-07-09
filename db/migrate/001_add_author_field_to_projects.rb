79841adc0642418317192f0c898aa72f37bcbc81
Jakub Motycka <jakubonty@centrum.cz>
2014-07-09 11:49:59 +0200

    Added author field migration

---
+++ db/migrate/001_add_author_field_to_projects.rb
@@ -0,0 +1,8 @@
+class AddAuthorFieldToProjects < ActiveRecord::Migration
+    def self.up
+        add_column :projects, :author_id, :integer
+    end
+    def self.down
+        remove_column :projects, :author_id
+    end
+end
